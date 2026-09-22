eb9f025fa97d7ff4212c149e33959b746ff87e40
joaofrangobr <joaorobertozucon85@gmail.com>
2026-09-22 19:58:27 -0300

    Botão delete
    Botão delete implementado

--- backend/OrdemServico.API/OrdemServico.API/obj/Debug/net10.0/OrdemServico.API.AssemblyInfo.cs
+++ backend/OrdemServico.API/OrdemServico.API/obj/Debug/net10.0/OrdemServico.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrdemServico.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5040cb80b3b1c9f1dec02c79df19b48c356e0397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675c8cd90ad34306b29e9fa1a2267ebd6578f6d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrdemServico.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrdemServico.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
